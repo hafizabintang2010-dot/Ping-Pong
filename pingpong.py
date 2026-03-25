96f912cb020d66aeea92936899b00b579c1abb66
hafizabintang2010-dot <hafizabintang2010@gmail.com>
2026-03-25 12:05:07 +0700

    create gamesprite class

--- pingpong.py
+++ pingpong.py
@@ -9,10 +9,27 @@ window.fill(background_color)
 game = True 
 clock = time.Clock()
 FPS = 60 
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, sprite_image, sprite_x, sprite_y, sprite_width, sprite_height, sprite_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image),(sprite_width, sprite_height))
+        self.speed = sprite_speed
+        self.rectangle = self.image.get_rect()
+        self.rectangle.x = sprite_x
+        self.rectangle.y = sprite_y
+
+    def reset(self): 
+        window.blit(self.image, (self.rectangle.x, self.rectangle.y))
+
+game_ball = GameSprite('ball.png', 200, 200, 50, 50, 4)
+
 while game: 
     for e in event.get():
         if e.type == QUIT:
             game = False 
+
+    game_ball.reset()
 
     display.update()
     clock.tick(FPS)
